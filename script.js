22639306c930b6be02fc299d06bb9f16dbd0f5a2
Tyler Serrato <21351@columbushs.com>
2019-09-18 12:18:10 -0400

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = 'black';
+}
+
+var penColor= 'black';
+
+function setPenColor(pen)
+{
+    penColor = pen;
+}
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor = penColor;
+}
